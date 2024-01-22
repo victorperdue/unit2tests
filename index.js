7c6c9a0cf4ab0c1af07b0e438d9f2b1ab1df95e3
victorperdue <victor.perdue@yahoo.com>
2024-01-21 20:29:30 -0700

    edits

---
+++ index.js
@@ -0,0 +1,19 @@
+//Test functions
+
+//Unit Tests
+//1.) Multiplacation: 
+//-Expect user to input (5,"abc") = expected error MessageChanne
+//-Expect user to input (3,7) = expect output to be 21
+//-Expect user to input (0,10) = expect output to be 0
+
+2.) concatOdds:
+-Expect user to input ([1,2,'abc'],[456]) = expect error MessageChanne
+-Expect user to input ([3,2,1],[9,1,1,1,4,15,-1]) = expect output of [-1,1,3,9,15]
+-Expect user to input ( , ) = expect out of ( )
+
+3.) Shopping cart checkout:
+- user tries to checkout an empty cart = expect error message saying "shopping cart is empty, please select an item to purchase"
+- user tries to checkout using guest profile = expect message asking to login or proceed with guest checkout. 
+- user tries to checkout under their profile = expect program to take users credentials upon check out and proceed to payment options.
+
+
